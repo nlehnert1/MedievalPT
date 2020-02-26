8250898eeccf867be62700720e2659bbf9ad3221
Chloe Lehnert <nlehnert2@unl.edu>
2020-02-26 15:30:04 -0600

    Added trigger plane to spawn new tomatoes once grabbed

---
+++ Assets/Scripts/ThrowableSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject tomato;
+    public GameObject self;
+    public void OnTriggerEnter(Collider other)
+    {
+        Instantiate(tomato, self.transform);
+    }
+}
