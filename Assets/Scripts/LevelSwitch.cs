beb05f8a1d23503ac62be7fb2e814a3f3a838ea4
JMNovotny <46601621+JMNovotny@users.noreply.github.com>
2020-03-06 15:21:54 -0600

    Scene loading is working, players are not

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LevelSwitch : MonoBehaviour
+{
+    public int currLevel = 0;
+
+    public string[] levelNames = new string[4] { "Hub", "TomatoThrowing", "RoyalWardrobe", "TrainingDummy"};
+
+    static LevelSwitch s = null;
+
+    void Start()
+    {
+        if (s == null)
+        {
+            s = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public void LevelSwitchHub()
+    {
+        currLevel = 0;
+        SteamVR_LoadLevel.Begin(levelNames[currLevel]);
+    }
+    
+    public void LevelSwitchTomato()
+    {
+        currLevel = 1;
+        SteamVR_LoadLevel.Begin(levelNames[currLevel]);
+    }
+
+    public void LevelSwitchWardrobe()
+    {
+        currLevel = 2;
+        SteamVR_LoadLevel.Begin(levelNames[currLevel]);
+    }
+
+    public void LevelSwitchDummy()
+    {
+        currLevel = 3;
+        SteamVR_LoadLevel.Begin(levelNames[currLevel]);
+    }
+}
